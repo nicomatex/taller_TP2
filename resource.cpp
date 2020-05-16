0813f3b6c668e8093ea5519e02ac8a9024439a10
Nicolas Aguerre <naguerre@fi.uba.ar>
2020-05-16 16:14:27 -0300

    Solucionado error de estilo

--- resource.cpp
+++ resource.cpp
@@ -1,5 +1,6 @@
-#include <string>
 #include "resource.h"
+
+#include <string>
 
 std::unordered_map<int, std::string> Resource::resource_names(NAMES_BY_ID);
 
@@ -9,7 +10,9 @@ Resource::Resource(const Resource& other) { this->id = other.id; }
 
 Resource::Resource(Resource&& other) { this->id = std::move(other.id); }
 
-std::string Resource::get_name_by_id(ResourceId id) { return resource_names[id]; }
+std::string Resource::get_name_by_id(ResourceId id) {
+    return resource_names[id];
+}
 
 std::string Resource::get_name() { return get_name_by_id(this->id); }
 
